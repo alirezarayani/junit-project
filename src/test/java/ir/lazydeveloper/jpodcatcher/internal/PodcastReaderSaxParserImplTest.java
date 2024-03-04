1498672214c9f74e00ee5b3828565b4ca0a11d26
alireza.rayani@gmail.com <alireza.rayani@gmail.com>
2024-03-04 15:12:55 +0330

    Add xml rss podcast and write first test class

--- src/test/java/ir/lazydeveloper/jpodcatcher/internal/PodcastReaderSaxParserImplTest.java
+++ src/test/java/ir/lazydeveloper/jpodcatcher/internal/PodcastReaderSaxParserImplTest.java
@@ -1,18 +1,87 @@
 package ir.lazydeveloper.jpodcatcher.internal;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import ir.lazydeveloper.jpodcatcher.PodcastReaderException;
+import ir.lazydeveloper.jpodcatcher.model.Channel;
+import ir.lazydeveloper.jpodcatcher.model.Item;
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class PodcastReaderSaxParserImplTest {
 
-    @BeforeEach
-    void setUp() {
-        //This method runs before each testcase
+    static PodcastReaderSaxParserImpl podcastReaderSaxParser;
+    static String samplePodcastURI = PodcastReaderSaxParserImplTest.class.getClassLoader().getResource("simple_podcast.xml").toString();
+
+    /**
+     * This method must be static
+     * And runs once
+     */
+    @BeforeAll
+    static void beforeAll() {
+        podcastReaderSaxParser = new PodcastReaderSaxParserImpl();
     }
 
+    /**
+     * This method runs before each testcase
+     */
+    @BeforeEach
+    void setUp() {
+    }
+
+    /**
+     * This method runs after each testcase
+     */
     @AfterEach
     void tearDown() {
-        //This method runs after each testcase
+    }
+
+    /**
+     * Each unit test must have @Test annotation
+     * must be void
+     * minimum visibility is package private
+     * cannot be abstract
+     */
+    @Test
+    void loadWrongRSSURIShouldThrowException() {
+        assertThrows(PodcastReaderException.class, () -> podcastReaderSaxParser.loadRSS("WRONG_URI"));
+    }
+
+    @Test
+    void loadRSSShouldContainChannelDate() throws PodcastReaderException {
+        Channel channel = podcastReaderSaxParser.loadRSS(samplePodcastURI);
+        assertAll(
+                () -> assertEquals("Raw Data", channel.title()),
+                () -> assertEquals("http://www.rawdatapodcast.com", channel.link()),
+                () -> assertEquals("We’ve entered a new era.", channel.description()),
+                () -> assertEquals("Thu, 21 Nov 2019 09:00:00 -0000", channel.pubDate()),
+                () -> assertEquals("Wed, 17 Mar 2021 19:22:02 -0000", channel.lastBuildDate()),
+                () -> assertEquals("en", channel.language()),
+                () -> assertEquals("All rights reserved", channel.copyright()),
+                () -> assertEquals("PRX Feeder v1.0.0", channel.generator()),
+                () -> assertEquals("https://f.prxu.org/190/images/RawData_ForWeb_RGB.png", channel.image().url()),
+                () -> assertEquals("Raw Data", channel.image().title()),
+                () -> assertEquals("http://www.rawdatapodcast.com", channel.image().link())
+        );
+    }
+
+    @Test
+    void loadRSSShouldContainEpisodesData() throws PodcastReaderException {
+        Channel channel = podcastReaderSaxParser.loadRSS(samplePodcastURI);
+        assertEquals(2, channel.items().size(), "the size of items must be equal two");
+        Item firstItem = channel.items().get(0);
+        assertAll(
+                () -> assertEquals("b970-9f45620b0fd1", firstItem.guid()),
+                () -> assertEquals("Technically Sweet", firstItem.title()),
+                () -> assertEquals("Thu, 21 Nov 2019 09:00:00 -0000", firstItem.pubDate()),
+                () -> assertEquals("https://beta.prx.org/stories/295275", firstItem.link()),
+                () -> assertNotNull(firstItem.enclosure()),
+                () -> assertEquals("https://dts.podtrac.com/Technically_Sweet_P1_Raw_Data.mp3", firstItem.enclosure().url()),
+                () -> assertEquals("audio/mpeg", firstItem.enclosure().type()),
+                () -> assertEquals(39374396L, firstItem.enclosure().length()),
+                () -> assertEquals(Arrays.asList("Blockchain", "Charity ryerson"), firstItem.categories())
+        );
     }
 
 }
